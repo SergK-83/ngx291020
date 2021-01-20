57e9f20d06ffca7c8e49173a1d18798984f0265b
Крюков Сергей Вадимович <ksv@sproject.local>
2021-01-20 16:58:41 +0700

    store begining

---
+++ src/app/store/actions/products.actions.ts
@@ -0,0 +1,8 @@
+import {createAction, props} from '@ngrx/store';
+
+export const getProductsPending = createAction('[Products module] pending products from server');
+export const getProductsSuccess =
+  createAction('[Products module] set products from server', props<{products: any}>());
+export const getProductsError = createAction('[Products module] error when try products from server');
+
+getProductsSuccess({products: []})
