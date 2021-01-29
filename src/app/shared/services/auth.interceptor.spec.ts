ee8aea7fb630c2e673ffdb4783299117b9f72ecd
Сергей Крюков <krus-83@mail.ru>
2021-01-29 09:53:17 +0700

    test auth.interceptor.spec.ts

--- src/app/shared/services/auth.interceptor.spec.ts
+++ src/app/shared/services/auth.interceptor.spec.ts
@@ -1,16 +1,51 @@
 import {products} from '../../data';
-import {HttpTestingController} from '@angular/common/http/testing';
-import {TestBed} from '@angular/core/testing';
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {inject, TestBed} from '@angular/core/testing';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {AuthInterceptor} from './auth.interceptor';
+import {BASE_URL} from './config';
+import {environment} from '../../../environments/environment';
+import {ProductsService} from '../../content/backoffice/content/products/products.service';
 
-const productsList = products;
+const mockedProducts = products;
 
 describe('Auth Interceptor', () => {
-  let httmMocked: HttpTestingController;
+  let httpMocked: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule],
+      imports: [HttpClientTestingModule],
+      providers: [
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: AuthInterceptor,
+          multi: true
+        },
+        {
+          provide: BASE_URL,
+          useValue: environment.baseUrl,
+          // multi: true
+        },
+        ProductsService
+      ]
     });
+    httpMocked = TestBed.inject(HttpTestingController);
+
+    it('should transform', inject([ProductsService, BASE_URL],
+      (productsService: ProductsService, baseUrl: string) => {
+        productsService.getProducts()
+          .subscribe((products) => {
+            expect(products).toEqual(mockedProducts);
+          });
+
+        const httpObj = httpMocked.expectOne({
+          method: 'GET',
+          url: `${baseUrl}/products`
+        });
+
+        httpObj.flush({data: mockedProducts});
+      }));
   });
+
+
 });
